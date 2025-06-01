291b51917a1426fc8bc5bec613f2caa07ab1119c
JoshuaBrien <joshuabrieno@gmail.com>
2025-06-02 00:25:54 +0800

    Update schtasks_exe.ps1

--- Windows/hidden/schtasks_exe.ps1
+++ Windows/hidden/schtasks_exe.ps1
@@ -2,4 +2,4 @@ $time = (Get-Date).AddMinutes(1) | Get-Date -Format "HH:mm"
 $date = Get-Date -Format "dd/MM/yyyy"
 Write-output $date
 Write-output $time
-schtasks /CREATE /TN "RavenOneTimeTask" /TR "C:\Windows\System32\notepad.exe" /SC ONCE /SD $date /ST $time
+schtasks /CREATE /TN "RavenOneTimeTask" /TR "C:\Windows\System32\notepad.exe" /SC ONCE /SD $date /ST $time /F
